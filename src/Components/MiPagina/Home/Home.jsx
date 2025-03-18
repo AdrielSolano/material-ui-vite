a330399e262d4b46891fcc8cecfe783f6f5497a4
AdrielSolano <2302019@utrivieramaya.edu.mx>
2025-03-18 11:54:01 -0500

    s

--- src/Components/MiPagina/Home/Home.jsx
+++ src/Components/MiPagina/Home/Home.jsx
@@ -7,7 +7,8 @@ const PageHome = () => {
     <div className="page-home">
       <div className="page-home-content">
         <h1>"Bienvenido a Mixología Express.</h1>
-        <p>Sumérgete en el mundo de los cócteles y descubre recetas únicas para cada ocasión. <tr/>  Aprende a preparar bebidas irresistibles, utilizando ingredientes frescos y técnicas fáciles de seguir.</p>
+        <p>Sumérgete en el mundo de los cócteles y descubre recetas únicas para cada ocasión.</p> 
+        <p>Aprende a preparar bebidas irresistibles, utilizando ingredientes frescos y técnicas fáciles de seguir.</p>
         <p>¡Conviértete en el alma de la fiesta y eleva tus habilidades detrás de la barra, un cóctel a la vez!"</p>
         <Link to="/recetas">
           <button>Conocer más</button>
