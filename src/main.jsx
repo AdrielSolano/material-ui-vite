ccbed41deb09598ae6bc3ef48f9c510aee2de2f9
AdrielSolano <2302019@utrivieramaya.edu.mx>
2025-03-18 09:04:44 -0500

    ss

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import theme from './theme';
+import theme from "@theme";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
